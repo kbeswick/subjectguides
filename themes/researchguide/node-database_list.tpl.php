68f3897cb555728b1b8105e20286ae6e70559ad3
Dan Scott <dscott@laurentian.ca>
2012-10-24 13:12:05 -0400

    Database lists: Save vertical space
    Hide the database description by default and enable users to toggle the
    description into existence. This saves significant vertical space and
    hopefully makes guide maintainers more likely to use proper database
    lists instead of HTML nodes.
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- themes/researchguide/node-database_list.tpl.php
+++ themes/researchguide/node-database_list.tpl.php
@@ -1,24 +1,35 @@
 <?php 
-	//need for each loop printing database info
-	foreach ($field_databases as $dbase_node)
-	{
-		$dbase = node_load($dbase_node['nid']);
-    //if the database is expired, do not show it
-    if (isset($dbase->field_database_expiration_date[0]['value2']) && strtotime($dbase->field_database_expiration_date[0]['value2']) < strtotime(date('Y-m-d')))
-      continue;
+    //need for each loop printing database info
+    foreach ($field_databases as $dbase_node)
+    {
+        $nid = $dbase_node['nid'];
+        $dbase = node_load($nid);
+        //if the database is expired, do not show it
+        if (isset($dbase->field_database_expiration_date[0]['value2']) && strtotime($dbase->field_database_expiration_date[0]['value2']) < strtotime(date('Y-m-d')))
+            continue;
 
-		// START Output for database
+        $dburl = $dbase->field_database_url[0]['url'];
+        $desc = $dbase->field_database_description[0]["value"];
+        $db_display = '';
+
+        if ($desc) {
+            $desc = <<<HERE
+                <tr id="dbdesc_$nid" style="display:none;">
+                    <td width="25%"><b>Description:</b></td>
+                    <td>$desc</td>
+                </tr>
+HERE;
+            $db_display = "<sup onclick='$(\"#dbdesc_$nid\").toggle();' style='text-decoration:underline;'>(?)</sup>";
+        }
+        // START Output for database
 ?>
 
-		<table border="0" class="db_listing">
-			<tr><td colspan="2"><a href="<?php print $dbase->field_database_url[0]['url']; ?>"><h3><?php print $dbase->title; ?></h3></a></td></tr>
-				<tr>
-					<td width="25%"><b>Database Description:</b></td>
-					<td><?php print $dbase->field_database_description[0]["value"]; ?></td>
-				</tr>
-			</table>
+        <table border="0" class="db_listing">
+            <tr><td colspan="2"><h3><a href="<?php print $dburl; ?>"><?php print $dbase->title; ?></a> <?php print $db_display; ?></h3></td></tr>
+            <?php print $desc; ?>
+        </table>
 
 <?php
-		// END Output for database
-	}
+        // END Output for database
+    }
 ?>
