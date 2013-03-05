df6f3f7a6d563c124d44cc485d200ff3edef94de
Dan Scott <dscott@laurentian.ca>
2013-03-05 00:20:27 -0500

    Add database list anchors and optional proxy support
    Break up the navigation of our 200+ databases by inserting anchors at
    strategic points in the alphabetical list, and offer that navigation at
    the break points.
    
    Also implement support for proxying URLs in database lists if the proxy
    boolean is true.
    
    Also, for sanity, format the SQL query for the main database list over
    multiple lines. There, isn't that easier to read? :)
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- themes/researchguide/node--1759.tpl.php
+++ themes/researchguide/node--1759.tpl.php
@@ -14,18 +14,55 @@
    *   3. Published
    */
 
-  $result = db_query("SELECT n.created, n.title, n.nid, n.changed FROM {node} n WHERE n.type = 'library_database' AND (n.language = :language OR n.language = 'und') AND n.status = 1 ORDER BY n.title ASC", array(':language' => $language->language));
+  $result = db_query("
+    SELECT n.created, n.title, n.nid, n.changed
+    FROM {node} n
+    WHERE n.type = 'library_database'
+      AND (n.language = :language OR n.language = 'und')
+      AND n.status = 1
+      ORDER BY n.title ASC
+    ", array(':language' => $language->language)
+  );
+
+  // Inline navigation via the wonder of anchors
+  $jump_text = 'Jump to:';
+  if ($language->language == 'fr') {
+    $jump_text = 'Aller à :';
+  }
+  $jump_list = <<<"HERE"
+    <p class='dblist_nav'>$jump_text
+      <a href="#list_A">A-C</a>
+      <a href="#list_D">D-G</a>
+      <a href="#list_H">H-L</a>
+      <a href="#list_M">M-R</a>
+      <a href="#list_S">S-Z</a>
+    </p>
+HERE;
+
 ?>
 <ul>
 <?php
 
+  // Place anchors roughly 1/5 of the way through the page
+  $anchors = array('A', 'D', 'H', 'M', 'S', 'Z');
+  $init_ltr = array_shift($anchors);
+
   foreach ($result as $i) {
+    $list_id = '';
     $nid = $i->nid;
     // Load the node contents for each database
     $cur_db_node = node_load($i->nid);
     // Grab the URL & description
+    $cur_db_title = $i->title;
     $cur_db_url = $cur_db_node->field_database_url['und'][0]['url'];
     $cur_db_desc = $cur_db_node->field_database_description['und'][0]['value'];
+    $cur_db_multi = $cur_db_node->field_multidisciplinary['und'][0]['value'];
+
+    // Proxy 'em if you got 'em
+    $cur_db_proxy = $cur_db_node->field_proxied['und'][0]['value'];
+    if ($cur_db_proxy) {
+      $cur_db_url = "http://librweb.laurentian.ca/login?url=$cur_db_url";
+    }
     $db_display = '';    
     $desc = '';
 
@@ -39,9 +76,16 @@
       $db_display = "(<span onclick='jQuery(\"[name=\\\"dbdesc_$nid\\\"]\").toggle();' style='font-weight: bold'>?</span>)";
 
     }
+
+    // Define an anchor, perhaps
+    if (substr($cur_db_title, 0, 1) == $init_ltr) {
+      $list_id = " id='list_$init_ltr'";
+      $init_ltr = array_shift($anchors);
+      print $jump_list;
+    }
 ?>
 
-    <li><a href="<?php print $cur_db_url; ?>"><?php print render($i->title); ?></a> <?php print render($db_display); ?></li>
+    <li<?php print $list_id; ?>><a href="<?php print $cur_db_url; ?>"><?php print render($cur_db_title); ?></a> <?php print render($db_display); ?></li>
     <?php print render($desc); ?>
 
 <?php
