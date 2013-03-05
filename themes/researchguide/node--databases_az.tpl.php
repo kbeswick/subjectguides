6ea8afea7c4697a54323305b4b94b78d173563dd
Kevin Beswick <kevinbeswick00@gmail.com>
2013-03-05 13:44:16 -0500

    More az database list template changes

--- themes/researchguide/node--databases_az.tpl.php
+++ themes/researchguide/node--databases_az.tpl.php
@@ -2,11 +2,32 @@
 // $Id: node.tpl.php,v 1.7 2007/08/07 08:39:36 goba Exp $
 ?>
   <div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
-    <h1>Database List</h1>
+
 
 <?php
 
   global $language;
+  global $base_url;
+
+  $multidis = FALSE;
+
+  // Check to see if we are listing all databases, or only multidisciplinary
+  if (strstr($_SERVER['REQUEST_URI'], 'multidisciplinary-databases') == 'multidisciplinary-databases') {
+    $multidis = TRUE;
+  }
+
+  // Choose the correct title to display based on language / multidisciplinary
+  $title_text = 'Databases A-Z';
+  if ($multidis)
+    $title_text = 'Multidisciplinary Databases';
+  if ($language->language == 'fr') {
+    if ($multidis)
+      $title_text = 'Bases de Données Multidisciplinaire';
+    else
+      $title_text = 'Bases de Données, A-Z';
+  }
+
+  print '<h1>' . $title_text . '</h1>';
 
   /*  We want all nodes that are:
    *   1. Of the type library_database
@@ -58,12 +79,18 @@ HERE;
     $cur_db_desc = $cur_db_node->field_database_description['und'][0]['value'];
     $cur_db_multi = $cur_db_node->field_multidisciplinary['und'][0]['value'];
 
+    // Skip this database if we are listing multidis & this db is not multidis
+    if ($multidis && !$cur_db_multi) {
+       continue;
+    }
+
     // Proxy 'em if you got 'em
     $cur_db_proxy = $cur_db_node->field_proxied['und'][0]['value'];
     if ($cur_db_proxy) {
       $cur_db_url = "http://librweb.laurentian.ca/login?url=$cur_db_url";
     }
-    $db_display = '';    
+    $db_display = '';
+    $db_edit = '';
     $desc = '';
 
     if ($cur_db_desc) {
@@ -77,6 +104,11 @@ HERE;
 
     }
 
+    //Check current user's roles and show an edit button if admin or librarian
+    if (in_array(librarian, $GLOBALS['user']->roles) || in_array(administrator, $GLOBALS['user']->roles)) {
+       $db_edit = '&nbsp;<a class="db_edit_link" href="' . $base_url . '/node/' . $nid . '/edit">(Edit)</a>';
+    }
+
     // Define an anchor, perhaps
     if (substr($cur_db_title, 0, 1) == $init_ltr) {
       $list_id = " id='list_$init_ltr'";
@@ -85,7 +117,7 @@ HERE;
     }
 ?>
 
-    <li<?php print $list_id; ?>><a href="<?php print $cur_db_url; ?>"><?php print render($cur_db_title); ?></a> <?php print render($db_display); ?></li>
+    <li<?php print $list_id; ?>><a href="<?php print $cur_db_url; ?>"><?php print render($cur_db_title); ?></a> <?php print render($db_display); ?><?php print render($db_edit); ?> </li>
     <?php print render($desc); ?>
 
 <?php
