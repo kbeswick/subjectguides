b5097e53f423038b55822b09df6e3ba1f4497e8d
Dan Scott <dscott@laurentian.ca>
2013-03-05 16:58:09 -0500

    More database list types: free trials, new databases
    Not 100% sure about the page names but worth a try. Also be finicky
    about things like having braces around if blocks, those tend to bite you
    when you add another statement to the would-be block.
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- themes/researchguide/node--databases_az.tpl.php
+++ themes/researchguide/node--databases_az.tpl.php
@@ -9,22 +9,32 @@
   global $language;
   global $base_url;
 
-  $multidis = FALSE;
-
-  // Check to see if we are listing all databases, or only multidisciplinary
-  if (strstr($_SERVER['REQUEST_URI'], 'multidisciplinary-databases') == 'multidisciplinary-databases') {
-    $multidis = TRUE;
+  $page_type = 'a-z';
+  $title_text = 'Databases A-Z';
+  if ($language->language == 'fr') {
+    $title_text = 'Bases de données, A-Z';
   }
 
-  // Choose the correct title to display based on language / multidisciplinary
-  $title_text = 'Databases A-Z';
-  if ($multidis)
+  // Check for other kinds of database lists
+  // This assumes that we don't wind up with a URL like "free-new-multidisciplinary"
+  if (strstr($_SERVER['REQUEST_URI'], 'multidisciplin')) {
+    $page_type = 'multidis';
     $title_text = 'Multidisciplinary Databases';
-  if ($language->language == 'fr') {
-    if ($multidis)
-      $title_text = 'Bases de Données Multidisciplinaire';
-    else
-      $title_text = 'Bases de Données, A-Z';
+    if ($language->language == 'fr') {
+      $title_text = 'Bases de données multidisciplinaire';
+    }
+  } elseif (strstr($_SERVER['REQUEST_URI'], 'free') or strstr($_SERVER['REQUEST_URI'], 'gratuit')) {
+    $page_type = 'trials';
+    $title_text = 'Databases - free trials';
+    if ($language->language == 'fr') {
+      $title_text = 'Bases de données : essais gratuits';
+    }
+  } elseif (strstr($_SERVER['REQUEST_URI'], 'new') or strstr($_SERVER['REQUEST_URI'], 'nouveau')) {
+    $page_type = 'new';
+    $title_text = 'New databases';
+    if ($language->language == 'fr') {
+      $title_text = 'Nouvelles bases de données';
+    }
   }
 
   print '<h1>' . $title_text . '</h1>';
@@ -78,14 +88,25 @@ HERE;
     $cur_db_url = $cur_db_node->field_database_url['und'][0]['url'];
     $cur_db_desc = $cur_db_node->field_database_description['und'][0]['value'];
     $cur_db_multi = $cur_db_node->field_multidisciplinary['und'][0]['value'];
+    $cur_db_proxy = $cur_db_node->field_proxied['und'][0]['value'];
+    $cur_db_new = $cur_db_node->field_new['und'][0]['value'];
+    $cur_db_expires = $cur_db_node->field_database_expiration_date['und'][0]['value2'];
 
-    // Skip this database if we are listing multidis & this db is not multidis
-    if ($multidis && !$cur_db_multi) {
+    // Skip the database if it is expired
+    if ($cur_db_expires && (strtotime($cur_db_expires) < strtotime(date('Y-m-d')))) {
+       continue;
+    }
+
+    // Skip the database if it does not match a requested page type
+    if ($page_type == 'multidis' && !$cur_db_multi) {
+       continue;
+    } elseif ($page_type == 'new' && !$cur_db_new) {
+       continue;
+    } elseif ($page_type == 'trials' && !$cur_db_expires) {
        continue;
     }
 
     // Proxy 'em if you got 'em
-    $cur_db_proxy = $cur_db_node->field_proxied['und'][0]['value'];
     if ($cur_db_proxy) {
       $cur_db_url = "http://librweb.laurentian.ca/login?url=$cur_db_url";
     }
@@ -94,17 +115,15 @@ HERE;
     $desc = '';
 
     if ($cur_db_desc) {
-
       $desc = '<p name="dbdesc_' . $nid . '" style="display:none;">';
       $desc .= '<span width="25%"><b>Description:</b></span>';
       $desc .= '<span>' . $cur_db_desc . '</span>';
       $desc .= '</p>';
 
       $db_display = "(<span onclick='jQuery(\"[name=\\\"dbdesc_$nid\\\"]\").toggle();' style='font-weight: bold'>?</span>)";
-
     }
 
-    //Check current user's roles and show an edit button if admin or librarian
+    // Check current user's roles and show an edit button if admin or librarian
     if (in_array(librarian, $GLOBALS['user']->roles) || in_array(administrator, $GLOBALS['user']->roles)) {
        $db_edit = '&nbsp;<a class="db_edit_link" href="' . $base_url . '/node/' . $nid . '/edit">(Edit)</a>';
     }
